cd40e793314022335ac5c26cc07dfbc82fd267ef
Max <kirienko.max@gmail.com>
2018-10-02 10:17:41 +0300

    antlr4

---
+++ generated/IFDLexer.h
@@ -0,0 +1,62 @@
+
+// Generated from /Users/max/dev/shout/src/IFD.g4 by ANTLR 4.7.1
+
+#pragma once
+
+
+#include "antlr4-runtime.h"
+
+
+namespace shout {
+
+
+class  IFDLexer : public antlr4::Lexer {
+public:
+  enum {
+    T__0 = 1, T__1 = 2, T__2 = 3, T__3 = 4, T__4 = 5, T__5 = 6, T__6 = 7, 
+    T__7 = 8, T__8 = 9, T__9 = 10, T__10 = 11, T__11 = 12, T__12 = 13, T__13 = 14, 
+    T__14 = 15, T__15 = 16, COMMENT = 17, STREAM = 18, TYPE = 19, OBJECT = 20, 
+    VERSION = 21, VAR_NAME = 22, VALUE = 23, INTEGER = 24, NUMBER = 25, 
+    STRING = 26, NO_QUOTED = 27, QUOTED = 28, CHARS = 29, WS = 30
+  };
+
+  IFDLexer(antlr4::CharStream *input);
+  ~IFDLexer();
+
+  virtual std::string getGrammarFileName() const override;
+  virtual const std::vector<std::string>& getRuleNames() const override;
+
+  virtual const std::vector<std::string>& getChannelNames() const override;
+  virtual const std::vector<std::string>& getModeNames() const override;
+  virtual const std::vector<std::string>& getTokenNames() const override; // deprecated, use vocabulary instead
+  virtual antlr4::dfa::Vocabulary& getVocabulary() const override;
+
+  virtual const std::vector<uint16_t> getSerializedATN() const override;
+  virtual const antlr4::atn::ATN& getATN() const override;
+
+private:
+  static std::vector<antlr4::dfa::DFA> _decisionToDFA;
+  static antlr4::atn::PredictionContextCache _sharedContextCache;
+  static std::vector<std::string> _ruleNames;
+  static std::vector<std::string> _tokenNames;
+  static std::vector<std::string> _channelNames;
+  static std::vector<std::string> _modeNames;
+
+  static std::vector<std::string> _literalNames;
+  static std::vector<std::string> _symbolicNames;
+  static antlr4::dfa::Vocabulary _vocabulary;
+  static antlr4::atn::ATN _atn;
+  static std::vector<uint16_t> _serializedATN;
+
+
+  // Individual action functions triggered by action() above.
+
+  // Individual semantic predicate functions triggered by sempred() above.
+
+  struct Initializer {
+    Initializer();
+  };
+  static Initializer _init;
+};
+
+}  // namespace shout
